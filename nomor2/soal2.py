827e17c6f3fbebd8121b23b62a628586c63269a7
javendzk <javana.dz@gmail.com>
2025-05-09 17:20:22 +0700

    comments dan restructure nomo r

--- nomor2/soal2.py
+++ nomor2/soal2.py
@@ -4,18 +4,22 @@
 import pandas as pandas 
 import numpy as numpy  
 
-csv_path = "../DataTugasPemrogramanA.csv"
-
-df = pandas.read_csv(csv_path)
-dataBersih = df.dropna()
-
-# menagambil data dari .csv
-x = dataBersih['Year'].values - 1960
-netY = dataBersih['Percentage_Internet_User'].values
-popY = dataBersih['Population'].values
-
-koefisienInternet = numpy.polyfit(x, netY, 3)
-koefisienPopulation = numpy.polyfit(x, popY, 3)
+def process_data(csv_path):
+    # read csv kemudian ambil datanya sesuai dengan kolom
+    # setelah itu, ya menghitung polinimial memakai numfit
+    df = pandas.read_csv(csv_path)
+    dataBersih = df.dropna()
+    
+    x = dataBersih['Year'].values - 1960
+    netY = dataBersih['Percentage_Internet_User'].values
+    popY = dataBersih['Population'].values
+    
+    # ini adalah numpy polyfit yang dipakai, saya menggunakan
+    # referensi pada https://numpy.org/doc/stable/reference/generated/numpy.polyfit.html 
+    koefisienInternet = numpy.polyfit(x, netY, 3)
+    koefisienPopulation = numpy.polyfit(x, popY, 3)
+    
+    return koefisienInternet, koefisienPopulation
 
 # memformat output (dengan bantuan AI GPT)
 def formatPoly(coefs, var='x'):
@@ -32,7 +36,13 @@ def formatPoly(coefs, var='x'):
             terms.append(f"{cStr}{var}^{power}")    
     return "y = " + " + ".join(terms)
 
+csv_path = "../DataTugasPemrogramanA.csv"
+koefisienInternet, koefisienPopulation = process_data(csv_path)
+    
 hasilInternet = formatPoly(koefisienInternet)
 hasilPopulation = formatPoly(koefisienPopulation)
-
-hasilInternet, hasilPopulation
+    
+print("polinomial internet:")
+print(hasilInternet)
+print("\polinomial Population:")
+print(hasilPopulation)
