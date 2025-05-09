b4a57de7a1c3213adfd8b4b3e0cbbff9b8dca8ea
Rivi <riviyasha@gmail.com>
2025-05-09 17:03:49 +0700

    complete comments number 3

--- nomor3/estimasi.py
+++ nomor3/estimasi.py
@@ -11,16 +11,22 @@ def predict_future_population_and_internet(csv_path):
 
 
     # Menggunakan regresi polinomial derajat 3 untuk memodelkan data populasi dan internet
+    # np.polyfit adalah fungsi dari NumPy yang digunakan untuk melakukan regresi polinomial
+    # years adalah x values, population dan internet adalah y values
+    # 3 adalah derajat polinomial yang digunakan
+    # Dalam hal ini, kita menggunakan derajat 3 untuk mendapatkan kurva yang lebih halus
     pop_coeffs = np.polyfit(years, population, 3)  
     net_coeffs = np.polyfit(years, internet, 3)    
 
 
     # Membuat fungsi polinomial berdasarkan koefisien yang diperoleh
+    # Mengubah array koefisien menjadi fungsi polinomial yang bisa dievaluasi seperti fungsi matematika.
     pop_poly = np.poly1d(pop_coeffs)  
     net_poly = np.poly1d(net_coeffs)  
 
 
-    # Memprediksi populasi pada tahun 2030 dan persentase pengguna internet pada tahun 2035
+    # Memprediksi populasi menggunakan fungsi polinomial yang telah dibuat
+    # pada tahun 2030 dan persentase pengguna internet pada tahun 2035
     pred_2030_pop = int(pop_poly(2030))  
     pred_2035_net = round(net_poly(2035), 2) 
 
